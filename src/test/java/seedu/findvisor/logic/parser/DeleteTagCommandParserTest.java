fb3c1602dbc54c12da33519a574b455a973fceed
YuZizhen <yuzizhen123@gmail.com>
2024-03-27 10:58:37 +0800

    Update DeleteTagCommandParserTest.java

--- src/test/java/seedu/findvisor/logic/parser/DeleteTagCommandParserTest.java
+++ src/test/java/seedu/findvisor/logic/parser/DeleteTagCommandParserTest.java
@@ -1,16 +1,16 @@
 package seedu.findvisor.logic.parser;
 
 import static seedu.findvisor.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-//import static seedu.findvisor.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.findvisor.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.findvisor.logic.parser.CommandParserTestUtil.assertParseFailure;
-//import static seedu.findvisor.logic.parser.CommandParserTestUtil.assertParseSuccess;
-//import static seedu.findvisor.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.findvisor.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.findvisor.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-//import seedu.findvisor.commons.core.index.Index;
+import seedu.findvisor.commons.core.index.Index;
 import seedu.findvisor.logic.commands.DeleteTagCommand;
-//import seedu.findvisor.model.tag.Tag;
+import seedu.findvisor.model.tag.Tag;
 
 public class DeleteTagCommandParserTest {
     private DeleteTagCommandParser parser = new DeleteTagCommandParser();
@@ -22,15 +22,11 @@ public class DeleteTagCommandParserTest {
                 DeleteTagCommand.MESSAGE_USAGE));
     }
 
-    // @Test
-    // public void parse_validArgs_returnsDeleteTagCommand() {
-    // Index targetIndex = INDEX_FIRST_PERSON;
-    // Tag targetTag = new Tag("friends");
-    // DeleteTagCommand expectedDeleteTagCommand = new DeleteTagCommand(targetIndex,
-    // targetTag);
-    // assertParseSuccess(parser,
-    // targetIndex.getOneBased() + " "
-    // + PREFIX_TAG + "friends",
-    // expectedDeleteTagCommand);
-    // }
+    @Test
+    public void parse_validArgs_returnsDeleteTagCommand() {
+        Index targetIndex = INDEX_FIRST_PERSON;
+        Tag targetTag = new Tag("friends");
+        DeleteTagCommand expectedDeleteTagCommand = new DeleteTagCommand(targetIndex, targetTag);
+        assertParseSuccess(parser, targetIndex.getOneBased() + " " + PREFIX_TAG + "friends", expectedDeleteTagCommand);
+    }
 }
