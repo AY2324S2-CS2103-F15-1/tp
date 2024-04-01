b775085c6506c121d2de8566020fdd06109455d3
YuZizhen <yuzizhen123@gmail.com>
2024-04-01 12:27:04 +0800

    Update HelpWindow.java

--- src/main/java/seedu/findvisor/ui/HelpWindow.java
+++ src/main/java/seedu/findvisor/ui/HelpWindow.java
@@ -16,7 +16,40 @@ import seedu.findvisor.commons.core.LogsCenter;
 public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL = "https://ay2324s2-cs2103-f15-1.github.io/tp/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
+    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL + "\n\n";
+
+    public static final String GENERAL_MESSAGE = "Below are the major commands for FINDvisor:\n";
+
+    public static final String CLEAR_COMMAND_MESSAGE = "Clear Command: Clear all the contacts in FINDvisor\n";
+    public static final String EXIT_COMMAND_MESSAGE = "Exit Command: Exit FINDvisor\n";
+    public static final String LIST_COMMAND_MESSAGE = "List Command: List all persons in FINDvisor.\n";
+    public static final String HELP_COMMAND_MESSAGE = "Help Command: Shows program usage instructions.\n";
+
+    public static final String ADD_COMMAND_MESSAGE = "Add Command: Adds a person to FINDvisor.\n";
+    public static final String DELETE_COMMAND_MESSAGE = "Delete Command: Deletes the person identified"
+            + " by the index number used in the displayed person list.\n";
+    public static final String FIND_COMMAND_MESSAGE = "Find Command: Finds all persons whose information matches "
+            + "the specified keywords (case-insensitive) of the specified category and "
+            + "displays them as a list with index numbers.\n";
+    public static final String EDIT_COMMAND_MESSAGE = "Edit Command: Edits the details of the person identified "
+            + "by the index number used in the displayed person list. "
+            + "Existing values will be overwritten by the input values.\n";
+
+    public static final String ADD_TAG_COMMAND_MESSAGE = "Add Tag Command: Adds new tags to the person identified"
+            + " by the index number used in the displayed person list.\n";
+    public static final String DELETE_TAG_COMMAND_MESSAGE = "Delete Tag Command: Deletes the tag associated with "
+            + "a person identified by the index number used in the displayed person list.\n";
+
+    public static final String SCHEDULE_COMMAND_MESSAGE = "Schedule Command: Schedules a meeting with the person "
+            + "identified by the index number used in the displayed person list.\n";
+    public static final String UNSCHEDULE_COMMAND_MESSAGE = "Unschedule Command: Unschedules a meeting with the person "
+            + "identified by the index number used in the displayed person list.\n";
+    public static final String RESCHEDULE_COMMAND_MESSAGE = "Reschedule Command: Reschedules a meeting with the person "
+            + "identified by the index number used in the displayed person list.\n";
+
+    public static final String REMARK_COMMAND_MESSAGE = "Remark Command: Updates a remark of the person identified "
+            + "by the index number used in the displayed person list. "
+            + "Existing values will be overwritten by the input.\n";
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
@@ -34,7 +67,12 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
-        helpMessage.setText(HELP_MESSAGE);
+        String message = HELP_MESSAGE + GENERAL_MESSAGE + HELP_COMMAND_MESSAGE + LIST_COMMAND_MESSAGE
+                + CLEAR_COMMAND_MESSAGE + EXIT_COMMAND_MESSAGE + ADD_COMMAND_MESSAGE + DELETE_COMMAND_MESSAGE
+                + FIND_COMMAND_MESSAGE + EDIT_COMMAND_MESSAGE + SCHEDULE_COMMAND_MESSAGE
+                + UNSCHEDULE_COMMAND_MESSAGE + RESCHEDULE_COMMAND_MESSAGE + ADD_TAG_COMMAND_MESSAGE
+                + DELETE_TAG_COMMAND_MESSAGE + REMARK_COMMAND_MESSAGE;
+        helpMessage.setText(message);
     }
 
     /**
@@ -47,20 +85,22 @@ public class HelpWindow extends UiPart<Stage> {
     /**
      * Shows the help window.
      * @throws IllegalStateException
-     *     <ul>
-     *         <li>
-     *             if this method is called on a thread other than the JavaFX Application Thread.
-     *         </li>
-     *         <li>
-     *             if this method is called during animation or layout processing.
-     *         </li>
-     *         <li>
-     *             if this method is called on the primary stage.
-     *         </li>
-     *         <li>
-     *             if {@code dialogStage} is already showing.
-     *         </li>
-     *     </ul>
+     *                               <ul>
+     *                               <li>
+     *                               if this method is called on a thread other than
+     *                               the JavaFX Application Thread.
+     *                               </li>
+     *                               <li>
+     *                               if this method is called during animation or
+     *                               layout processing.
+     *                               </li>
+     *                               <li>
+     *                               if this method is called on the primary stage.
+     *                               </li>
+     *                               <li>
+     *                               if {@code dialogStage} is already showing.
+     *                               </li>
+     *                               </ul>
      */
     public void show() {
         logger.fine("Showing help page about the application.");
